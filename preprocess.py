be52d3f21146777bf0352f6ac09f63023bcb545d
jannik-reissfelder <jannik.reissfelder@gmail.com>
2024-06-15 11:55:01 +0200

    adjust preprocess

--- preprocess.py
+++ preprocess.py
@@ -74,7 +74,6 @@ def mixup_data(X, Y, alpha=1, mix_up_x=False, degree=1):
 class PreprocessingClass:
     def __init__(self,
                  hold_out_species: str,
-                 mapping: dict,
                  data: pd.DataFrame,
                  feature_length: int,
                  use_augmentation: bool = False,
@@ -86,14 +85,12 @@ class PreprocessingClass:
         self.hold_out_species = hold_out_species
         self.data = data
         self.feature_length = feature_length
-        self.mapping = mapping
         self.use_augmentation = use_augmentation
         self.use_normalized_data = use_normalized_data
         self.candidate_n_neighbors = candidate_n_neighbors
         self.mix_up_x = mix_up_x
         self.X = None
         self.Y = None
-        self.Y_candidates = None
         self.closest = None
 
     def set_hold_out_set(self):
@@ -101,7 +98,7 @@ class PreprocessingClass:
         This function sets the hold out set.
         """
         # remove the hold out species from the distance matrix columns
-        self.data.drop(columns=[self.hold_out_species], inplace=True)
+        # self.data.drop(columns=[self.hold_out_species], inplace=True)
         # removing the  hold out  species from the row data
         # first assign it to the hold_out_set
         self.hold_out_set = self.data.loc[[self.hold_out_species]]
@@ -120,111 +117,38 @@ class PreprocessingClass:
         self.set_hold_out_set()
 
         # Splitting the dataframe into features and targets
-        self.X = self.data.iloc[:, :(self.feature_length -1)]
-        self.Y = self.data.iloc[:, (self.feature_length -1):]
+        self.X = self.data.iloc[:, :(self.feature_length )]
+        self.Y = self.data.iloc[:, (self.feature_length ):]
         print("X-shape:", self.X.shape)
         print("Y-shape:", self.Y.shape)
 
         # do same for hold out set
-        self.X_hold_out = self.hold_out_set.iloc[:, :(self.feature_length -1)]
-        self.Y_hold_out = self.hold_out_set.iloc[:, (self.feature_length -1):]
+        self.X_hold_out = self.hold_out_set.iloc[:, :(self.feature_length)]
+        self.Y_hold_out = self.hold_out_set.iloc[:, (self.feature_length):]
         print("X_hold_out-shape:", self.X_hold_out.shape)
         print("Y_hold_out-shape:", self.Y_hold_out.shape)
 
-    def get_candidate_microbes(self):
-        """
-        This function gets the candidate microbes according to the closest 
-        neihbors of the hold out species.
-        """
-        # get all micros set
-        total_micros = set(self.Y.columns)
-        print("Total number of microbes found:", len(total_micros))
-        hold_out_inst = self.X_hold_out.iloc[0]
-        candidate_neighbors = hold_out_inst[self.X.index.unique()]
-        # find closest species to hold out species
-        self.closest = find_closest_species(candidate_neighbors, self.candidate_n_neighbors)
-        print("closest species found: ", self.closest)
-        candidate_rows = self.Y.loc[self.closest]
-        # determine the candidate micros
-        # self.candidate_micros = candidate_rows.loc[:, (candidate_rows != 0).any(axis=0)].columns # TODO enable when using this logic again
-        self.candidate_micros = self.Y.columns # this is just getting all of them again
-        # assign the candidate micros to Y_candidates from Y
-        self.Y_candidates = self.Y[self.candidate_micros]
-        # also re-assign Y_hold_out to Y_candidates_hold_out
-        self.Y_candidates_hold_out = self.Y_hold_out[self.candidate_micros]
-        # get difference between total micros and candidate micros
-        self.non_candidates = total_micros - set(self.candidate_micros)
-        print("Number of candidate microbes found:", len(self.candidate_micros))
-        print("Number of non-candidate microbes found:", len(self.non_candidates))
-        print("Y_candidates-shape:", self.Y_candidates.shape)
-        print("X-shape:", self.X.shape)
-
-    def normalize_microbiome_data(self):
-        """
-        Normalize microbiome data using the specified method within Pandas DataFrame.
-
-        Parameters:
-        - data_frame: Pandas DataFrame
-            Microbiome data matrix where samples are rows and OTUs are columns.
-        - method: str
-            Normalization method ("TSS", "CSS", "DESeq", "TMM", "GMPR", "absolute").
-
-        Returns:
-        - normalized_data_frame: Pandas DataFrame
-            Normalized microbiome data matrix.
-        """
-
-        # Check if the method is valid
-        valid_methods = ["TSS", "CSS", "DESeq", "TMM", "absolute"]
-        if self.use_normalized_data not in valid_methods:
-            raise ValueError("Invalid normalization method. Please choose from: {}".format(", ".join(valid_methods)))
-
-        # Absolute: No normalization
-        if self.use_normalized_data == "absolute":
-            self.Y = self.Y
-
-        # TSS Normalization
-        elif self.use_normalized_data == "TSS":
-            scale_factor = self.Y.sum(axis=1)
-            self.Y = self.Y.div(scale_factor, axis=0)
-
-        # CSS Normalization
-        elif self.use_normalized_data == "CSS":
-            scale_factor = self.Y.apply(lambda x: x.sum() / np.median(x[x > 0]), axis=0)
-            self.Y = self.Y.div(scale_factor, axis=1)
-
-        # DESeq Normalization
-        elif self.use_normalized_data == "DESeq":
-            scale_factor = self.Y.apply(lambda x: x.sum() / np.exp(np.mean(np.log(x[x > 0]))), axis=0)
-            self.Y = self.Y.div(scale_factor, axis=1)
 
     def mixup_by_subspecies(self):
         '''Applies Mixup augmentation to the dataset within each subspecies.'''
 
         # Initialize empty DataFrames to hold the augmented data
         augmented_X = pd.DataFrame(columns=self.X.columns)
-        augmented_Y = pd.DataFrame(columns=self.Y_candidates.columns)
+        augmented_Y = pd.DataFrame(columns=self.Y.columns)
 
         # Get unique subspecies from the index of Y_candidates
-        subspecies = self.Y_candidates.index.unique()
+        subspecies = self.Y.index.unique()
 
         # Iterate through each subspecies (without the holdout species)
         for species in subspecies:
-            # check whether the species is in the mapping and if not continue to the next species
-            # reason: if species is not in mapping no need to augment
-            if species not in self.mapping:
-                continue
-
-            species_specific_degree = self.mapping[species]
 
             # Locate the rows for the current subspecies
-            species_mask = self.Y_candidates.index == species
+            species_mask = self.Y.index == species
             self.X_sub = self.X[species_mask]
-            self.Y_sub = self.Y_candidates[species_mask]
+            self.Y_sub = self.Y[species_mask]
 
             # Apply mixup_data function to the current subspecies
-            mixed_X_sub, mixed_Y_sub = mixup_data(self.X_sub, self.Y_sub, mix_up_x=self.mix_up_x,
-                                                  degree=species_specific_degree)
+            mixed_X_sub, mixed_Y_sub = mixup_data(self.X_sub, self.Y_sub, mix_up_x=self.mix_up_x)
 
             # Append the mixed data to the augmented DataFrames
             augmented_X = pd.concat([augmented_X, mixed_X_sub])
@@ -240,7 +164,7 @@ class PreprocessingClass:
 
         # create a column called 'source' and set it to 'original'
         self.X['source'] = 'original'
-        self.Y_candidates['source'] = 'original'
+        self.Y['source'] = 'original'
 
     def augment_data_if_needed(self):
         '''
@@ -254,7 +178,7 @@ class PreprocessingClass:
             print("Data augmentation applied.")
             # concatenate the original data with the augmented data
             self.X_final = pd.concat([self.X, self.mixed_X])
-            self.Y_candidates_final = pd.concat([self.Y_candidates, self.mixed_Y])
+            self.Y_candidates_final = pd.concat([self.Y, self.mixed_Y])
             # drop the source columns for both
             self.X_final.drop(columns=['source'], inplace=True)
             self.Y_candidates_final.drop(columns=['source'], inplace=True)
@@ -266,7 +190,7 @@ class PreprocessingClass:
             print("Data augmentation not applied; use_augmentation is set to False.")
             # reassing X_final and Y_candidates_final to X and Y_candidates
             self.X_final = self.X
-            self.Y_candidates_final = self.Y_candidates
+            self.Y_candidates_final = self.Y
 
     def run_all_methods(self):
         """
@@ -274,6 +198,4 @@ class PreprocessingClass:
         """
 
         self.preprocesses_seed_data()
-        self.get_candidate_microbes()
-        self.normalize_microbiome_data()
         self.augment_data_if_needed()
